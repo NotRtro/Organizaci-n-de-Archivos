63d74505223d114382ab89013dd527d692c1286c
Angel-Utec <91230666+Angel-Utec@users.noreply.github.com>
2023-05-04 12:44:14 -0500

    Update parser.h

--- parser/parser.h
+++ parser/parser.h
@@ -27,19 +27,82 @@ std::vector<Token> tokenize(std::string query) {
         if (c == ' ') {
         }
 
-            // Si encontramos un paréntesis, lo añadimos como un token separado.
+        // Si encontramos un paréntesis, lo añadimos como un token separado.
         else if (c == '(' || c == ')') {
             std::string value;
             value.push_back(c);
             tokens.push_back(Token(Token::Operator, value));
         }
 
-            // Si encontramos una coma, la añadimos como un token separado.
+        // Si encontramos una coma, la añadimos como un token separado.
         else if (c == ',') {
             std::string value;
             value.push_back(c);
             tokens.push_back(Token(Token::Operator, value));
         }
 
-            // Si encontramos un operador, lo añadimos como un token separado.
-        else if (c 
+        // Si encontramos un operador, lo añadimos como un token separado.
+        else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '=' || c == '>' || c == '<') {
+            std::string value;
+            value.push_back(c);
+            tokens.push_back(Token(Token::Operator, value));
+        }
+
+        // Si encontramos comillas dobles, leemos una cadena hasta la siguiente comilla doble.
+        else if (c == '"') {
+            size_t start = i + 1;
+            size_t end = query.find('"', start);
+            if (end == std::string::npos) {
+                end = query.size();
+            }
+            std::string value = query.substr(start, end - start);
+            tokens.push_back(Token(Token::Number, value));
+        }
+
+        // Si encontramos un dígito, leemos un número entero o decimal.
+        else if (std::isdigit(c)) {
+            int cont = 0;
+            std::string value = "";
+            while(query[i] != ' ' && query[i] != '(' && query[i] != ')' && query[i] != ',' && query[i] != '+' && query[i] != '-' && query[i] != '*' && query[i] != '/' && query[i] != '%' && query[i] != '>' && query[i] != '<' && query[i] != '=' && query[i] != '!'){
+                if(i >= query.size()){
+                    break;
+                }
+                cont++;
+                value.push_back(query[i]);
+                i++;
+            }
+            i--;
+            tokens.push_back(Token(Token::Number, value));
+        }
+
+        else {
+            // Si no es ninguna de las anteriores, leemos una palabra hasta el siguiente espacio en blanco o símbolo.
+            int cont = 0;
+            std::string value = "";
+            while(query[i] != ' ' && query[i] != '(' && query[i] != ')' && query[i] != ',' && query[i] != '+' && query[i] != '-' && query[i] != '*' && query[i] != '/' && query[i] != '%' && query[i] != '>' && query[i] != '<' && query[i] != '=' && query[i] != '!'){
+                if(i >= query.size()){
+                    break;
+                }
+                cont++;
+                value.push_back(query[i]);
+                i++;
+            }
+            i--;
+            if (value == "SELECT" || value == "INSERT" || value == "UPDATE" || value == "DELETE" || value == "FROM" || value == "WHERE" || value == "ORDER" || value == "BY" || value == "AND" || value == "OR"){
+                tokens.push_back(Token(Token::Identifier, value));
+            }
+            else{
+                tokens.push_back(Token(Token::Keyword, value));
+            }
+        }
+    }
+
+    return tokens;
+}
+
+void Consulta(vector<Token> Tokens){
+    for (const auto& token : Tokens) {
+
+    }
+}
+
