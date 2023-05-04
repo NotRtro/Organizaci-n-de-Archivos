376ad5b149bf6644257ce727277122ef6c72041a
Angel-Utec <91230666+Angel-Utec@users.noreply.github.com>
2023-05-04 14:29:52 -0500

    Parser casi terminado

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -3,12 +3,13 @@
 #include <vector>
 #include <algorithm>
 #include <cctype>
+#include "../lib/Hash.hpp"
 
 using namespace std;
 
 // Definimos una estructura para representar un token, que puede ser una palabra clave, un identificador, un número o un operador.
 struct Token {
-    enum Type { Keyword, Identifier, Number, Operator } type;
+    enum Type { Keyword, Identifier, Number, Operator, Value } type;
     std::string value;
 
     Token(Type type, std::string value) : type(type), value(value) {}
@@ -50,13 +51,19 @@ std::vector<Token> tokenize(std::string query) {
 
         // Si encontramos comillas dobles, leemos una cadena hasta la siguiente comilla doble.
         else if (c == '"') {
-            size_t start = i + 1;
-            size_t end = query.find('"', start);
-            if (end == std::string::npos) {
-                end = query.size();
+            i++;
+            int cont = 0;
+            std::string value = "";
+            while(query[i] != '"'){
+                if(i >= query.size()){
+                    break;
+                }
+                cont++;
+                value.push_back(query[i]);
+                i++;
             }
-            std::string value = query.substr(start, end - start);
-            tokens.push_back(Token(Token::Number, value));
+            i--;
+            tokens.push_back(Token(Token::Value, value));
         }
 
         // Si encontramos un dígito, leemos un número entero o decimal.
@@ -88,7 +95,7 @@ std::vector<Token> tokenize(std::string query) {
                 i++;
             }
             i--;
-            if (value == "SELECT" || value == "INSERT" || value == "UPDATE" || value == "DELETE" || value == "FROM" || value == "WHERE" || value == "ORDER" || value == "BY" || value == "AND" || value == "OR"){
+            if (value == "SELECT" || value == "INSERT" || value == "UPDATE" || value == "DELETE" || value == "TO" || value == "FROM" || value == "WHERE" || value == "ORDER" || value == "BY" || value == "AND" || value == "OR"){
                 tokens.push_back(Token(Token::Identifier, value));
             }
             else{
@@ -100,9 +107,60 @@ std::vector<Token> tokenize(std::string query) {
     return tokens;
 }
 
-void Consulta(vector<Token> Tokens){
-    for (const auto& token : Tokens) {
-
+/*void SELECT(vector<Token> tokens, Hash temp){
+    if(tokens[1].value == '*'){
+        temp.get();
     }
+}*/
+
+void INSERT(vector<Token> tokens, Hash temp){
+    if(tokens[3].value != "Tienda"){
+        return cout<<"No admitido"<<endl;
+    }
+    vector<string> valores;
+    for (int j = 5; j < tokens.size(); j++) {
+        if (tokens[j].type == Token::Keyword){
+            valores.push_back(tokens[j]);
+        }
+    }
+    temp.set(valores[0], valores[1], valores[2], stoi(valores[3]), stoi(valores[4]), valores[5]);
+    cout<<"se hizo la insercion"<<endl;
+};
+
+/*void UPDATE(vector<Token> tokens, Hash temp){
+
+}*/
+
+void DELETE(vector<Token> tokens, Hash temp){
+    if (tokens[2].value != "Tienda"){
+        cout<<"No admitido"<<endl;
+        return;
+    }
+    string key_delete = "";
+    for (int i = 0; i < tokens.size(); ++i) {
+        if (tokens[i].type == Token::Value){
+            key_delete = tokens[i];
+        }
+    }
+    temp.remove(key_delete);
+    cout<<"Eliminado"<<endl;
 }
 
+void Consulta(vector<Token> Tokens){
+    Hash temp("Data_from_Structurs/dataHash.dat");
+    /*if(Tokens[0].value == "SELECT"){
+        return SELECT(Tokens,temp);
+    }*/
+    else if(Tokens[0].value == "INSERT"){
+        return INSERT(Tokens,temp);
+    }
+    else if(Tokens[0].value == "UPDATE"){
+        return UPDATE(Tokens,temp);
+    }
+    else if(Tokens[0].value == "DELETE"){
+        return DELETE(Tokens,temp);
+    }
+    else{
+        cout<<"no es una consulta admitida"
+    }
+}
